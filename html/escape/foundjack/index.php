be55ef71c9a3abe70cbd5407bcf872da95304c53
nimpo <dr.mike.jones@gmail.com>
2025-03-24 10:51:36 +0000

    Use errormsg() instead of manual error page; Add value of coordinates posted as answer to 404 if answer doesn't checkout; removed old commented message.

--- html/escape/foundjack/index.php
+++ html/escape/foundjack/index.php
@@ -9,9 +9,7 @@ include 'questions.inc';
 # Test Group
 #
 if ( $groupname == false ) {
-  http_response_code(503);
-  echo '<h2 class="subtitle">Error 503</h2><div class="content-container"><p>Cannot find groupname!</p></div>';
-  exit;
+  errormsg("503","Cannot find groupname!");
 }
 
 ##################
@@ -22,10 +20,9 @@ if ( checkJack() ) {
   echo "<p>".$questionCongrats['Jack'][array_rand($questionCongrats['Jack'])]."</p>";
   echo "<p>".$questionsStay[array_rand($questionsStay)]."</p>";
   echo "</div></div>";
-#  echo '<h2 class="subtitle">Jack found safe and sound</h2><div class="content-container"><div class="scroll-content" id="scrollable"><p>Well done shipmates! Jack is safe and is recovering in sick bay. You have access to a chart and have instructions to return to harbour.</p><p>However, as you head back, you hear a witchy song on the air. You are inexplicably drawn towards some haunting music. <br /><a href="/escape/sirens/">Click here and escape the sirens song</a></p></div></div>';
 }
 else {
-  errormsg("404","Ah, ye wee skallyway. Those by the tactics most unworthy of a Sea Scout. What's the first law of scouting: A scout needs be trustworthy. No you mayn't continue onward. You'll be turnin' about. Savvy!");
+  errormsg("404","Ah, ye wee skallyway. Submitting those ill gotten coordinates: ".$_POST['lat'].",".$_POST['lng'].", be the tactics most unworthy of a Sea Scout. What's the first law of scouting: A scout needs be trustworthy. No you mayn't continue onward. You'll be turnin' about. Savvy!");
 }
 ?>
     <div class="bottom-links">
