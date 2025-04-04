2a53667ba42a4e130a44b0cbfa752bb8ba1d1278
nimpo <dr.mike.jones@gmail.com>
2025-04-04 17:33:48 +0100

    Some way towards better cookie management. Added nocookie youtube but autoplay in one instance might make that moot there.

--- html/help/transits/index.php
+++ html/help/transits/index.php
@@ -2,7 +2,7 @@
     <h2 class="subtitle">Help with transits</h2>
     <div class="content-container">
       <div class="scroll-content" id="scrollable" style="justify-content: center; padding-top:10px;">
-        <iframe style="margin: 0 auto; display: block; margin-bottom: 10px;" width="560" height="315" src="https://www.youtube.com/embed/zuRyzHX-KLY?si=8xnrsPfeuYPQHTud" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe style="margin: 0 auto; display: block; margin-bottom: 10px;" width="560" height="315" src="https://www.youtube-nocookie.com/embed/zuRyzHX-KLY?si=8xnrsPfeuYPQHTud" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <p>NB Depth measurements are affected by the height of the tide at the time. The Chart shows the depth at Lowest Astronomical Tide. The depth given has already been adjusted for tide height.</p>
       </div>
     </div>
