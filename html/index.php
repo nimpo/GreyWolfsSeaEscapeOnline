a1f3341e2d9a0b638a5bac74857494c94265ebb4
nimpo <dr.mike.jones@gmail.com>
2025-04-04 15:58:25 +0100

    For gradual opening.

--- html/index.php
+++ html/index.php
@@ -1,4 +1,10 @@
 <?php $h1=""; include 'buffer.inc'; ?>
+    <div class="content-container">
+      <div class="scroll-content" id="scrollable">
+        <h3>Under Construction</h3>
+        <p>This site is still undergoing tests and final tweeks. Certain areas remain locked. If you think you should be able to access some of the areas here but cannot, please contact <span aria-label="This email address is obfuscated. Please obtain my email address by other means." style="font-family: ScrambleN;">&lt;89;.$A@;:*&lt;#@:AB4D:FAB4:.A,+.B9</span>.</p>
+      </div>
+    </div>
     <div class="title-link">
       <h1>Grey Wolf's Sea Escape</h1>
       <p style="margin-block-start:2px"><a href="scouts/">Scouts&nbsp;click&nbsp;here</a> | <a href="leaders/">Leaders&nbsp;click&nbsp;here</a></p>
