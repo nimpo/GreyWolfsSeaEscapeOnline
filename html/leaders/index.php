87e82ca644252f61d2326ed3059cb5fed07346d9
nimpo <dr.mike.jones@gmail.com>
2025-04-15 09:26:04 +0100

    Added youtube overview to leaders section.

--- html/leaders/index.php
+++ html/leaders/index.php
@@ -5,6 +5,7 @@ include "buffer.inc";
     <div class="content-container">
       <div class="scroll-content" id="scrollable">
         <p>Welcome to the leaders portal. Should you wish to make use of the online components of this Sea Escape for your group, please register via the link below. The other links are, I hope, self evident.</p>
+        <iframe style="margin: 0 auto; display: block; margin-bottom: 10px;" width="560" height="315" src="https://www.youtube-nocookie.com/embed/jrouj3oCrXU?si=IdBeo0S6SlsknPBX&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     </div>
     <div class="bottom-links">
